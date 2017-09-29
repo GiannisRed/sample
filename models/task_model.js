bbfe45f056511add015640416a84b65ae6310418
Giannis Fatouros <fatouros@programize.com>
2017-09-29 14:23:20 +0300

    Add user ref on task model

--- models/task_model.js
+++ models/task_model.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 var taskSchema = mongoose.Schema({
     name: String,
     subject: String,
-    completed: { type: Boolean, default: false }
+    completed: { type: Boolean, default: false },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
 });
 
 module.exports = mongoose.model('Task', taskSchema);
